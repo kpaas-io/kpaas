26da6ca2401c67f4c65d9b041d73a094db47ee40
zyseeker <steven.zhangyong@outlook.com>
2019-11-27 17:02:21 +0800

    feature(deploy): add nodeCheckItem for node check action
    1. added nodeCheckItem
    2. added method to generate node check task stats based on is actions' status.

--- pkg/deploy/action/node_check_executor.go
+++ pkg/deploy/action/node_check_executor.go
@@ -33,14 +33,36 @@ func (a *nodeCheckExecutor) Execute(act Action) error {
 	if !ok {
 		return fmt.Errorf("the action type is not match: should be node check action, but is %T", act)
 	}
-	// TODO: implemented the node check logic
+
+	// The following codes are just for example
+	// 1. check kernel version
+	// TODO: ssh to check the kernel version
+	kernelVersionItem := &nodeCheckItem{
+		name:        "kernel version",
+		description: "kernel version must not less than 4.4.0",
+		status:      nodeCheckItemSucessful,
+	}
+	nodeCheckAction.checkItems = append(nodeCheckAction.checkItems, kernelVersionItem)
+
+	// 2. check docker version
+	// TODO: ssh to check the docker version
+	dockerVersionItem := &nodeCheckItem{
+		name:        "docker version check",
+		description: "docker version must not less than 17.03.02",
+		status:      nodeCheckItemFailed,
+		err: &pb.Error{
+			Reason:     "docker version is too low: 17.0.0",
+			Detail:     "",
+			FixMethods: "upgrade docker version to 17.03.02+",
+		},
+	}
+	nodeCheckAction.checkItems = append(nodeCheckAction.checkItems, dockerVersionItem)
+
+	// TODO: other checks
 
 	// TODO: update action status
-	nodeCheckAction.err = &pb.Error{
-		Reason:     "todo",
-		Detail:     "todo",
-		FixMethods: "todo",
-	}
+	nodeCheckAction.status = ActionFailed
+	nodeCheckAction.err = dockerVersionItem.err
 
 	logrus.Debugf("End to execute action: %+v", act)
 	return nil
