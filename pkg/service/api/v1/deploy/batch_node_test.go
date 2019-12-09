e5b5a0137c293d42bf027fee0f02e2a365286c24
Luckyboys <eacholwong@gmail.com>
2019-12-09 16:20:44 +0800

    feat(service-api): Added Batch upload node configuration file API

---
+++ pkg/service/api/v1/deploy/batch_node_test.go
@@ -0,0 +1,311 @@
+// Copyright 2019 Shanghai JingDuo Information Technology co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package deploy
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/kpaas-io/kpaas/pkg/constant"
+	"github.com/kpaas-io/kpaas/pkg/service/model/api"
+	"github.com/kpaas-io/kpaas/pkg/service/model/wizard"
+	"github.com/kpaas-io/kpaas/pkg/utils/h"
+)
+
+func TestUploadBatchNodes(t *testing.T) {
+
+	logrus.SetLevel(logrus.TraceLevel)
+	tests := []struct {
+		BaseNodeList  []*wizard.Node
+		Input         string
+		Want          *api.GetNodeListResponse
+		ResponseError *h.AppErr
+	}{
+		{
+			BaseNodeList: []*wizard.Node{},
+			Input: `
+#<hostname> <user>  <role,role,role>         <IP>             <ssh port>  <password>          <login key name>        <docker path>
+k8s-master1   root	    master,etcd          192.168.3.223    22          111111111111	      -		                  /var/lib/docker
+k8s-master2   root	    master,etcd          192.168.3.224    22          111111111111	      -   	                  /var/lib/docker
+`,
+			Want: &api.GetNodeListResponse{
+				Nodes: []api.NodeData{
+					{
+						NodeBaseData: api.NodeBaseData{
+							Name:                "k8s-master1",
+							MachineRoles:        []constant.MachineRole{constant.MachineRoleMaster, constant.MachineRoleEtcd},
+							DockerRootDirectory: "/var/lib/docker",
+							Labels:              []api.Label{},
+							Taints:              []api.Taint{},
+						},
+						ConnectionData: api.ConnectionData{
+							SSHLoginData: api.SSHLoginData{
+								Username:           "root",
+								AuthenticationType: api.AuthenticationTypePassword,
+								Password:           "",
+							},
+							IP:   "192.168.3.223",
+							Port: 22,
+						},
+					},
+					{
+						NodeBaseData: api.NodeBaseData{
+							Name:                "k8s-master2",
+							MachineRoles:        []constant.MachineRole{constant.MachineRoleMaster, constant.MachineRoleEtcd},
+							DockerRootDirectory: "/var/lib/docker",
+							Labels:              []api.Label{},
+							Taints:              []api.Taint{},
+						},
+						ConnectionData: api.ConnectionData{
+							SSHLoginData: api.SSHLoginData{
+								Username:           "root",
+								AuthenticationType: api.AuthenticationTypePassword,
+								Password:           "",
+							},
+							IP:   "192.168.3.224",
+							Port: 22,
+						},
+					},
+				},
+			},
+		},
+		{
+			BaseNodeList: []*wizard.Node{},
+			Input: `
+#<hostname> <user>  <role,role,role>         <IP>             <ssh port>  <password>          <login key name>        <docker path>
+k8s-master1   root	    master,etcd          192.168.3.223    22          -	      TestKey		                  /var/lib/docker
+k8s-master2   root	    master,etcd          192.168.3.224    22          -	      TestKey   	                  /var/lib/docker
+`,
+			Want: &api.GetNodeListResponse{
+				Nodes: []api.NodeData{
+					{
+						NodeBaseData: api.NodeBaseData{
+							Name:                "k8s-master1",
+							MachineRoles:        []constant.MachineRole{constant.MachineRoleMaster, constant.MachineRoleEtcd},
+							DockerRootDirectory: "/var/lib/docker",
+							Labels:              []api.Label{},
+							Taints:              []api.Taint{},
+						},
+						ConnectionData: api.ConnectionData{
+							SSHLoginData: api.SSHLoginData{
+								Username:           "root",
+								AuthenticationType: api.AuthenticationTypePrivateKey,
+								PrivateKeyName:     "TestKey",
+							},
+							IP:   "192.168.3.223",
+							Port: 22,
+						},
+					},
+					{
+						NodeBaseData: api.NodeBaseData{
+							Name:                "k8s-master2",
+							MachineRoles:        []constant.MachineRole{constant.MachineRoleMaster, constant.MachineRoleEtcd},
+							DockerRootDirectory: "/var/lib/docker",
+							Labels:              []api.Label{},
+							Taints:              []api.Taint{},
+						},
+						ConnectionData: api.ConnectionData{
+							SSHLoginData: api.SSHLoginData{
+								Username:           "root",
+								AuthenticationType: api.AuthenticationTypePrivateKey,
+								PrivateKeyName:     "TestKey",
+							},
+							IP:   "192.168.3.224",
+							Port: 22,
+						},
+					},
+				},
+			},
+		},
+		{
+			BaseNodeList: []*wizard.Node{
+				{
+					Name: "k8s-master1",
+				},
+			},
+			Input: `
+#<hostname> <user>  <role,role,role>         <IP>             <ssh port>  <password>          <login key name>        <docker path>
+k8s-master1   root	    master,etcd          192.168.3.223    22          111111111111	      -		                  /var/lib/docker
+`,
+			Want:          nil,
+			ResponseError: h.EExists.WithPayload(fmt.Sprintf("node name k8s-master1 was exist")),
+		},
+		{
+			BaseNodeList: []*wizard.Node{},
+			Input: `
+#<hostname> <user>  <role,role,role>         <IP>             <ssh port>  <password>          <login key name>        <docker path>
+k8s-master1   -	    master,etcd          192.168.3.223    22          111111111111	      -		                  /var/lib/docker
+`,
+			Want:          nil,
+			ResponseError: h.EParamsError.WithPayload(fmt.Sprintf(`"username" illegal`)),
+		},
+		{
+			BaseNodeList:  []*wizard.Node{},
+			Input:         ``,
+			Want:          nil,
+			ResponseError: h.EParamsError.WithPayload(fmt.Sprintf(`node list empty`)),
+		},
+		{
+			BaseNodeList: []*wizard.Node{},
+			Input: `
+k8s-master1   root	    master,etcd          192.168.3.223    22          111111111111	      -		                  /var/lib/docker
+k8s-master1   root	    master,etcd          192.168.3.224    22          111111111111	      -		                  /var/lib/docker
+`,
+			Want:          nil,
+			ResponseError: h.EParamsError.WithPayload(fmt.Sprintf(`node name k8s-master1 was duplicated`)),
+		},
+		{
+			BaseNodeList: []*wizard.Node{},
+			Input: `
+k8s-master1   root	    master,etcd          192.168.3.223    22          111111111111	      -		                  /var/lib/docker
+k8s-master2   root	    master,etcd          192.168.3.223    22          111111111111	      -		                  /var/lib/docker
+`,
+			Want:          nil,
+			ResponseError: h.EParamsError.WithPayload(fmt.Sprintf(`node ip 192.168.3.223 was duplicated`)),
+		},
+		{
+			BaseNodeList: []*wizard.Node{},
+			Input: `
+k8s-master1   root	    master,etcd          192.168.3.223    99999999999999999999999999999          111111111111	      -		                  /var/lib/docker
+`,
+			Want:          nil,
+			ResponseError: h.EParamsError.WithPayload(fmt.Sprintf(`strconv.Atoi: parsing "99999999999999999999999999999": value out of range`)),
+		},
+	}
+
+	for _, test := range tests {
+
+		wizard.ClearCurrentWizardData()
+		wizardData := wizard.GetCurrentWizard()
+		wizardData.Nodes = test.BaseNodeList
+		var err error
+		resp := httptest.NewRecorder()
+		gin.SetMode(gin.TestMode)
+		ctx, _ := gin.CreateTestContext(resp)
+		bodyReader := strings.NewReader(test.Input)
+		ctx.Request = httptest.NewRequest("POST", "/api/v1/deploy/wizard/batchnodes", bodyReader)
+
+		UploadBatchNodes(ctx)
+
+		resp.Flush()
+		assert.True(t, resp.Body.Len() > 0)
+		fmt.Printf("result: %s\n", resp.Body.String())
+		if test.Want != nil {
+			responseData := new(api.GetNodeListResponse)
+			err = json.Unmarshal(resp.Body.Bytes(), responseData)
+			assert.Nil(t, err)
+
+			assert.Equal(t, test.Want, responseData)
+		} else {
+			responseData := new(h.AppErr)
+			err = json.Unmarshal(resp.Body.Bytes(), responseData)
+			assert.Nil(t, err)
+
+			assert.Equal(t, test.ResponseError.Error(), responseData.Error())
+			assert.Equal(t, test.ResponseError.Status, resp.Code)
+		}
+	}
+}
+
+func TestTryToMatchBatchNodes(t *testing.T) {
+
+	tests := []struct {
+		Input          []byte
+		WantMatches    [][]string
+		WantGroupNames []string
+	}{
+		{
+			Input: []byte(`
+#<hostname> <user>  <role,role,role>         <IP>             <ssh port>  <password>          <login key name>        <docker path>
+k8s-master1   root	    master,etcd          192.168.3.223    22          111111111111	      -		                  /var/lib/docker
+`),
+			WantMatches: [][]string{
+				{
+					"k8s-master1   root	    master,etcd          192.168.3.223    22          111111111111	      -		                  /var/lib/docker",
+					"k8s-master1",
+					"root",
+					"master,etcd",
+					"192.168.3.223",
+					"22",
+					"111111111111",
+					"-",
+					"/var/lib/docker",
+				},
+			},
+			WantGroupNames: []string{
+				"",
+				"nodeName",
+				"username",
+				"roles",
+				"ip",
+				"port",
+				"password",
+				"privateKeyName",
+				"dockerPath",
+			},
+		},
+	}
+
+	for _, test := range tests {
+
+		matches, groupNames := tryToMatchBatchNodes(test.Input)
+		assert.Equal(t, test.WantMatches, matches)
+		assert.Equal(t, test.WantGroupNames, groupNames)
+	}
+}
+
+func TestSplitInputRoles(t *testing.T) {
+
+	tests := []struct {
+		Input map[string]string
+		Want  []constant.MachineRole
+	}{
+		{
+			Input: map[string]string{
+				"roles": "master,etcd",
+			},
+			Want: []constant.MachineRole{
+				constant.MachineRoleMaster,
+				constant.MachineRoleEtcd,
+			},
+		},
+		{
+			Input: map[string]string{
+				"roles": "worker,etcd",
+			},
+			Want: []constant.MachineRole{
+				constant.MachineRoleWorker,
+				constant.MachineRoleEtcd,
+			},
+		},
+		{
+			Input: map[string]string{
+				"roles": "",
+			},
+			Want: []constant.MachineRole{},
+		},
+	}
+
+	for _, test := range tests {
+		assert.Equal(t, test.Want, splitInputRoles(test.Input))
+	}
+}
---
+++ pkg/service/api/v1/deploy/batch_nodes.go
@@ -0,0 +1,223 @@
+// Copyright 2019 Shanghai JingDuo Information Technology co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package deploy
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+
+	"github.com/kpaas-io/kpaas/pkg/constant"
+	"github.com/kpaas-io/kpaas/pkg/service/model/api"
+	"github.com/kpaas-io/kpaas/pkg/service/model/wizard"
+	"github.com/kpaas-io/kpaas/pkg/utils/h"
+	"github.com/kpaas-io/kpaas/pkg/utils/log"
+)
+
+// @ID AddSSHCertificate
+// @Summary Add SSH login private key
+// @Description Add SSH login private key
+// @Tags nodes
+// @Accept text/plain
+// @Produce application/json
+// @Param nodes body string true "node list"
+// @Success 201 {object} api.GetNodeListResponse
+// @Router /api/v1/deploy/wizard/batchnodes [post]
+func UploadBatchNodes(c *gin.Context) {
+
+	requestData, err := getUploadBatchNodesRequestData(c)
+	if err != nil {
+		h.E(c, h.EParamsError.WithPayload(err.Error()))
+		log.ReqEntry(c).Infof("parameter error: %v, %T", err, err)
+		return
+	}
+
+	nodeList := make([]*wizard.Node, 0, len(*requestData))
+	for _, data := range *requestData {
+
+		node := wizard.NewNode()
+		node.Name = data.Name
+		node.DockerRootDirectory = data.DockerRootDirectory
+		node.MachineRoles = data.MachineRoles
+
+		node.IP = data.IP
+		node.Port = data.Port
+		node.Username = data.Username
+		node.AuthenticationType = convertAPIAuthenticationTypeToModelAuthenticationType(data.AuthenticationType)
+		switch data.AuthenticationType {
+		case api.AuthenticationTypePassword:
+			node.Password = data.Password
+		case api.AuthenticationTypePrivateKey:
+			node.PrivateKeyName = data.PrivateKeyName
+		}
+
+		nodeList = append(nodeList, node)
+	}
+
+	wizardData := wizard.GetCurrentWizard()
+	err = wizardData.AddNodeList(nodeList)
+	if err != nil {
+
+		h.E(c, err)
+		return
+	}
+
+	responseNodeList := getWizardNodes()
+	h.R(c, api.GetNodeListResponse{
+		Nodes: *responseNodeList,
+	})
+}
+
+func getUploadBatchNodesRequestData(c *gin.Context) (nodeList *[]*api.NodeData, err error) {
+
+	data, err := c.GetRawData()
+	log.ReqEntry(c).Tracef("rawData: %v, err: %v", string(data), err)
+	if err != nil {
+		return
+	}
+
+	/**
+	Excample Template
+	#<hostname> <user>  <role,role,role>         <IP>             <ssh port>  <password>          <login key name>        <docker path>
+	k8s-master1   root	    master,etcd          192.168.3.223    22          111111111111	      -		                  /var/lib/docker
+	k8s-master2   root	    master,etcd          192.168.3.224    22          111111111111	      -   	                  /var/lib/docker
+	k8s-master3   root	    master,etcd          192.168.3.227    22          111111111111	      -   	                  /var/lib/docker
+	k8s-worker1   root	    worker,etcd          192.168.3.226    22          -	                  worker_key 	          /var/lib/docker
+	k8s-worker2   root	    worker,etcd          192.168.3.229    22          -	                  worker_key 	          /var/lib/docker
+	k8s-worker3   root	    worker               192.168.3.230    22          -	                  worker_key 	          /var/lib/docker
+	*/
+	matches, groupNames := tryToMatchBatchNodes(data)
+
+	log.ReqEntry(c).Tracef("matches: %v", matches)
+
+	if len(matches) <= 0 {
+		err = fmt.Errorf("node list empty")
+		return
+	}
+
+	nodeList = new([]*api.NodeData)
+	*nodeList = make([]*api.NodeData, 0, len(matches))
+
+	ipList := make(map[string]bool)
+	nameList := make(map[string]bool)
+
+	for _, match := range matches {
+
+		matchMap := make(map[string]string)
+		for i, groupName := range groupNames {
+
+			if i > 0 && i <= len(match) {
+				matchMap[groupName] = match[i]
+			}
+		}
+
+		log.ReqEntry(c).Tracef("matched roles: %s", matchMap["roles"])
+		roles := splitInputRoles(matchMap)
+		log.ReqEntry(c).Tracef("roles: %#v", roles)
+
+		loginData := api.SSHLoginData{
+			Username:           matchMap["username"],
+			Password:           matchMap["password"],
+			AuthenticationType: api.AuthenticationTypePassword,
+		}
+
+		if matchMap["password"] == "-" && matchMap["privateKeyName"] != "-" {
+			loginData.AuthenticationType = api.AuthenticationTypePrivateKey
+			loginData.PrivateKeyName = matchMap["privateKeyName"]
+			loginData.Password = ""
+		}
+
+		var port int
+		port, err = strconv.Atoi(matchMap["port"])
+		if err != nil {
+			return
+		}
+
+		node := &api.NodeData{
+			NodeBaseData: api.NodeBaseData{
+				Name:                matchMap["nodeName"],
+				MachineRoles:        roles,
+				Labels:              nil,
+				Taints:              nil,
+				DockerRootDirectory: matchMap["dockerPath"],
+			},
+			ConnectionData: api.ConnectionData{
+				SSHLoginData: loginData,
+				IP:           matchMap["ip"],
+				Port:         uint16(port),
+			},
+		}
+		log.ReqEntry(c).Tracef("node: %#v", node)
+
+		err = node.Validate()
+		if err != nil {
+			return
+		}
+
+		if _, exist := ipList[matchMap["ip"]]; exist {
+
+			err = fmt.Errorf("node ip %s was duplicated", matchMap["ip"])
+			return
+		}
+
+		if _, exist := nameList[matchMap["nodeName"]]; exist {
+
+			err = fmt.Errorf("node name %s was duplicated", matchMap["nodeName"])
+			return
+		}
+
+		ipList[matchMap["ip"]] = true
+		nameList[matchMap["nodeName"]] = true
+
+		*nodeList = append(*nodeList, node)
+	}
+
+	return
+}
+
+func splitInputRoles(matchMap map[string]string) []constant.MachineRole {
+	roles := make([]constant.MachineRole, 0)
+	if rolesString, exist := matchMap["roles"]; exist {
+		logrus.WithField("func", "splitInputRoles").Tracef("rolesString: %s", rolesString)
+		splitRoles := strings.Split(rolesString, ",")
+		logrus.WithField("func", "splitInputRoles").Tracef("splitString: %s", splitRoles)
+		for _, role := range splitRoles {
+			trimmedRole := strings.TrimSpace(role)
+			if role == "" {
+				continue
+			}
+			roles = append(roles, constant.MachineRole(trimmedRole))
+		}
+	}
+	return roles
+}
+
+func tryToMatchBatchNodes(data []byte) ([][]string, []string) {
+	re := regexp.MustCompile(`(?m)^\s*` +
+		`(?P<nodeName>[\w_\-]+)\s+` +
+		`(?P<username>[\w_\-]+)\s+` +
+		`(?P<roles>[\w,]+)\s+` +
+		`(?P<ip>[\d.a-f:]+)\s+` +
+		`(?P<port>[\d]+)\s+` +
+		`(?P<password>[\w` + "`" + `~!@#$%^&*()_\-+=\\|\[\]{};:'", ./<>?]+)\s+` +
+		`(?P<privateKeyName>[\-\w]+)\s+` +
+		`(?P<dockerPath>[\w\-_\/]+)`,
+	)
+	return re.FindAllStringSubmatch(string(data), -1), re.SubexpNames()
+}
