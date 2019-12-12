061c9a80ec57f72c77e33102ffba95c6701bbdea
yangruiray <37774269+yangruiray@users.noreply.github.com>
2019-12-12 15:15:40 +0800

    feature(deploy/init): add executor, action, processor, task of deploy/init #OONE-11, OONE-12, OONE-13 (#54)
    * (WIP)feature(deploy/init): add executor, action, processor, task of deploy/init
    
    * fix(deploy/init): fix init framework passing parameter
    
    * refactor(deploy/init): refactor private var, func to public
    
    * fix(deploy/init): modify error return
    
    * fix(deploy/init): fix nodeInitProcessor

---
+++ pkg/deploy/action/node_init_action.go
@@ -0,0 +1,88 @@
+// Copyright 2019 Shanghai JingDuo Information Technology co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package action
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/sirupsen/logrus"
+
+	pb "github.com/kpaas-io/kpaas/pkg/deploy/protos"
+)
+
+const (
+	nodeInitItemPending = "pending"
+	nodeInitItemDoing   = "doing"
+	nodeInitItemDone    = "done"
+	nodeInitItemFailed  = "failed"
+)
+
+// NodeInitActionConfig represents the config for a node init action
+type NodeInitActionConfig struct {
+	Node            *pb.Node
+	LogFileBasePath string
+}
+
+type NodeInitAction struct {
+	Base
+	sync.RWMutex
+
+	Node      *pb.Node
+	InitItems []*NodeInitItem
+}
+
+type NodeInitItemStatus string
+
+type NodeInitItem struct {
+	Name        string
+	Description string
+	Status      NodeInitItemStatus
+	Err         *pb.Error
+}
+
+// NewNodeInitAction returns a node init action based on the config.
+// User should use this function to create a node init action.
+func NewNodeInitAction(cfg *NodeInitActionConfig) (Action, error) {
+	var err error
+	if cfg == nil {
+		err = fmt.Errorf("action config is nil")
+	} else if cfg.Node == nil {
+		err = fmt.Errorf("invalid config: node is nil")
+	}
+
+	if err != nil {
+		logrus.Error(err)
+		return nil, err
+	}
+
+	actionName := getNodeInitActionName(cfg)
+	return &NodeInitAction{
+		Base: Base{
+			Name:              actionName,
+			ActionType:        ActionTypeNodeInit,
+			Status:            ActionPending,
+			LogFilePath:       GenActionLogFilePath(cfg.LogFileBasePath, actionName),
+			CreationTimestamp: time.Now(),
+		},
+		Node: cfg.Node,
+	}, nil
+}
+
+// return node name as the action name, temporarily
+func getNodeInitActionName(cfg *NodeInitActionConfig) string {
+	return cfg.Node.GetName()
+}
---
+++ pkg/deploy/task/node_init_processor.go
@@ -0,0 +1,80 @@
+// Copyright 2019 Shanghai JingDuo Information Technology co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package task
+
+import (
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/kpaas-io/kpaas/pkg/deploy/action"
+	"github.com/kpaas-io/kpaas/pkg/deploy/consts"
+)
+
+// nodeInitProcessor implements the specific logic to init nodes
+type nodeInitProcessor struct{}
+
+// Spilt the task into one or more node init actions
+func (p *nodeInitProcessor) SplitTask(t Task) error {
+	if err := p.verifyTask(t); err != nil {
+		logrus.Errorf("invalid task: %s", err)
+		return err
+	}
+
+	logger := logrus.WithFields(logrus.Fields{
+		consts.LogFieldAction: t.GetName(),
+	})
+
+	logger.Debug("Start to split node init task")
+
+	initTask := t.(*NodeInitTask)
+
+	// split task into actions: will create a action for every node, the action type
+	// is NodeInitAction
+	actions := make([]action.Action, 0, len(initTask.Nodes))
+	for _, node := range initTask.Nodes {
+		actionCfg := &action.NodeInitActionConfig{
+			Node:            node,
+			LogFileBasePath: initTask.LogFilePath,
+		}
+		act, err := action.NewNodeInitAction(actionCfg)
+		if err != nil {
+			return err
+		}
+		actions = append(actions, act)
+	}
+	initTask.Actions = actions
+
+	logrus.Debugf("Finish to split node init task: %d actions", len(actions))
+	return nil
+}
+
+// Verify if the task is valid
+func (p *nodeInitProcessor) verifyTask(t Task) error {
+	if t == nil {
+		return consts.ErrEmptyTask
+	}
+
+	nodeInitTask, ok := t.(*NodeInitTask)
+	if !ok {
+		return fmt.Errorf("%s: %T", consts.MsgTaskTypeMismatched, t)
+	}
+
+	if len(nodeInitTask.Nodes) == 0 {
+		return fmt.Errorf("nodes is empty")
+	}
+
+	return nil
+}
