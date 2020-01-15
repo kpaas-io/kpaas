d0712fd9a6e3d4e0b9ca705fffa5e0e2ff49fb3b
congcongke <zhanweidu@163.com>
2020-01-15 14:39:25 +0800

    feat(deploy/log): write log to file for deploy etcd/join operation
    Signed-off-by: congcongke <zhanweidu@163.com>

--- pkg/deploy/action/join_master_executor.go
+++ pkg/deploy/action/join_master_executor.go
@@ -56,6 +56,7 @@ func (a *joinMasterExecutor) Execute(act Action) *pb.Error {
 		NeedUntaint:   needUntaint,
 		MasterNodes:   action.MasterNodes,
 		ClusterConfig: action.ClusterConfig,
+		LogWriter:     action.GetExecuteLogBuffer(),
 	}
 
 	op, err := master.NewJoinMasterOperation(config)
